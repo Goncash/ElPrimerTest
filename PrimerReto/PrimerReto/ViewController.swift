21e15f98d2ad8616cf077a50ce11877786fa9d96
Goncash <gonzalocastanedo1@gmail.com>
2016-05-17 11:26:24 -0500

    Add files via upload

---
+++ PrimerReto/PrimerReto/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PrimerReto
+//
+//  Created by Carolina Delgado Vergara on 17/05/16.
+//  Copyright © 2016 Gonzalo Castanedo Shaadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
